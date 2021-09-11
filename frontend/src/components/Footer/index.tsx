f4bb418d1c4aff18fadb2d0cd166aaa855d8e9ce
Rodolfo Marriel <rodolfo.29r@gmail.com>
2021-09-11 13:47:43 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">Desenvolvido por <a href="https://github.com/rodolfomarriel" target="_blank" rel="noreferrer">Rodolfo Marriel</a></p>
+        <p className="text-light"><small><strong>Spring Boot | React</strong><br />
+          Site pessoal: <a href="https://rodolfomarriel.me/" target="_blank" rel="noreferrer">rodolfomarriel.me</a></small></p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
