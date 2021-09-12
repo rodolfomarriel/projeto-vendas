320297f7cf87cd3b0cf363a81f6310a81f090354
Rodolfo Marriel <rodolfo.29r@gmail.com>
2021-09-12 11:56:05 -0300

    Home, navigation

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
       <div className="container">
         <p className="text-light">Desenvolvido por <a href="https://github.com/rodolfomarriel" target="_blank" rel="noreferrer">Rodolfo Marriel</a></p>
         <p className="text-light"><small><strong>Spring Boot | React</strong><br />
-          Site pessoal: <a href="https://rodolfomarriel.me/" target="_blank" rel="noreferrer">rodolfomarriel.me</a></small></p>
+          Contatos: <a href="https://rodolfomarriel.me/" target="_blank" rel="noreferrer">rodolfomarriel.me</a></small></p>
       </div>
     </footer>
   );
