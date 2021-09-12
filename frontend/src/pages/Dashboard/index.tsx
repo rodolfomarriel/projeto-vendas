2307585bfe14f3ef447cce8b36d800d606fdce2d
Rodolfo Marriel <rodolfo.29r@gmail.com>
2021-09-12 17:33:20 -0300

    BarChart integration

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,8 +1,8 @@
-import NavBar from "../../components/NavBar";
-import Footer from "../../components/Footer";
-import DataTable from "../../components/DataTable";
-import BarChart from "../../components/BarChart";
-import DonutChart from "../../components/DonutChart";
+import NavBar from "components/NavBar";
+import Footer from "components/Footer";
+import DataTable from "components/DataTable";
+import BarChart from "components/BarChart";
+import DonutChart from "components/DonutChart";
 
 const Dashboard = () => {
   return (
